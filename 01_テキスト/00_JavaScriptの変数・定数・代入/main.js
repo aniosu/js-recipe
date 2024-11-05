e9b98a43b204b9e4e0929c8b3eaa0bef25c7fd09
aniosu <ameniosu@au.com>
2024-11-06 02:36:03 +0900

    コンソール

--- 01_テキスト/00_JavaScriptの変数・定数・代入/main.js
+++ 01_テキスト/00_JavaScriptの変数・定数・代入/main.js
@@ -3,7 +3,11 @@ let myNickname = "エキスパくん"
 console.log(myNickname) // => "エキスパくん" と表示される
 
 // TRY: 変数 myNickname に再代入して自分のあだ名を表示してください
+myNickname = "ヒロト"
 
 console.log(myNickname)
 
 // TRY: myAge を定数で宣言して自分の年齢を表示してください
+const myAge = "18"
+
+console.log(myAge)
