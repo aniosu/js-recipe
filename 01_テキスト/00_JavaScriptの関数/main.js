9c57df8610076ffb4a431626cd14589627f37bc0
aniosu <ameniosu@au.com>
2024-11-06 16:57:36 +0900

    関数の基礎

--- 01_テキスト/00_JavaScriptの関数/main.js
+++ 01_テキスト/00_JavaScriptの関数/main.js
@@ -1,5 +1,3 @@
-/*const x=5
-const y=3
 // TRY: 練習問題 １
 const plus1 = function (x) {
   return x + 1
@@ -10,24 +8,23 @@ const plus1 = function (x) {
 
 // TRY: 練習問題 ２
 // f1: 1, 4, 7, 10, 13, 16, ...
-const f1 = function(x) {
-  return x+3
+const f1 = function (x) {
+  return x + 3
 }
 // f2: 1, 10, 100, 1000, 10000, ...
-const f2 = function(x) {
-  return x*10
+const f2 = function (x) {
+  return x * 10
 }
 // f3: 2, 14, 107, 1010, 10013, ...
-const f3 = function(x){
-  return f1+f2
+const f3 = function (x) {
+  return f1 + f2
 }
 // TRY: 練習問題３
 // ① ２つの引数を受け取り、それらをかけ合わせた値を返す関数 multiply を作成して正しく動くかテストしてください。
-const multiply = function(x,y){
-return x*y
+const multiply = function (x, y) {
+  return x * y
 }
 // ② ２つの引数を受け取り、それらを足し合わせた結果を四捨五入した値を返す関数 plusRound を作成して正しく動くかテストしてください。
-const plusRound = function (x,y){
-  return Math.round(x+y)
+const plusRound = function (x, y) {
+  return Math.round(x + y)
 }
-*/
