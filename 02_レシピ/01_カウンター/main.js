b2f9a29e02b71ac11dd0b1c792a079c2fe539deb
aniosu <ameniosu@au.com>
2024-11-09 17:58:46 +0900

    [カウンター](発展）を学習しました。

--- 02_レシピ/01_カウンター/main.js
+++ 02_レシピ/01_カウンター/main.js
@@ -1,9 +1,19 @@
 const display = document.getElementById("display")
 const plusButton = document.getElementById("plus-button")
+const minusButton = document.getElementById("minus-button")
+const doubleBottun = document.getElementById("double-button")
 let count = 0
 plusButton.onclick = function () {
   // count を更新
   count += 1
   // count を表示
+  display.textContent = count
+}
+minusButton.onclick = function () {
+  count -= 1
+  display.textContent = count
+}
+doubleBottun.onclick = function () {
+  count *= 2
   display.textContent = count
 }
