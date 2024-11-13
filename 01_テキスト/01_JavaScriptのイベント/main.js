d69c820e91e59cfb8bd91f7a6e56165a3ea9cad7
aniosu <ameniosu@au.com>
2024-11-13 11:54:17 +0900

    [と]を学習しました。

---
+++ 01_テキスト/01_JavaScriptのイベント/main.js
@@ -0,0 +1,22 @@
+// イベントが発生する要素を取得する
+const button = document.getElementById("button")
+
+const alertMessage = function () {
+  alert("クリックしたね")
+}
+
+// 関数を登録
+button.onclick = alertMessage
+// input 要素と input イベント
+const inputText = document.getElementById("input-text")
+const inputDate = document.getElementById("input-date")
+
+const logValue = function (e) {
+  console.log(e)
+}
+
+inputText.oninput = logValue
+inputDate.oninput = logValue
+document.onkeydown = function (e) {
+  console.log(e.key)
+}
