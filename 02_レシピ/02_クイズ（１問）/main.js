94ca163c35d2ebea8c830d592b9ea2798ca60c4d
aniosu <ameniosu@au.com>
2024-11-27 12:16:29 +0900

    11/27:12:16

---
+++ 02_レシピ/02_クイズ（１問）/main.js
@@ -0,0 +1,59 @@
+const quizText = document.getElementById("quiz-text")
+const quizImage = document.getElementById("quiz-image")
+const quizButton = document.getElementById("quiz-button")
+const feedback = document.getElementById("feedback")
+const choice = []
+// クイズの内容
+const quiz = {
+  text: "この星の名前は何でしょう？",
+  image: "Ganymede.jpg",
+  choices: [
+    {
+      text: "ゴリアテ",
+      feedback:
+        "残念！ゴリアテは、旧約聖書に登場するダビデに石で殺される巨人だよ。",
+    },
+    {
+      text: "ゼニガメ",
+      feedback: "残念！ゼニガメは、クサガメまたはニホンイシガメの幼体だよ。",
+    },
+    {
+      text: "ガニメデ",
+      feedback: "正解！ガニメデは、木星の第三惑星だよ！",
+    },
+  ],
+}
+
+// quiz を画面に表示する関数
+const reloadQuiz = function () {
+  // 問題文を表示
+  quizText.textContent = "Q. " + quiz.text
+
+  // 画像を表示
+  quizImage.src = "./images/" + quiz.image
+  for (let i = 0; i < quiz.choices.length; i++) {
+    choice[i] = document.createElement("button")
+    choice[i].textContent = quiz.choices[0].text
+    choice[i].append(quizButton)
+  }
+}
+
+// choiceNumber番目の選択肢を選択
+const choose = function (choiceNumber) {
+  // フィードバックを表示
+  feedback.textContent = quiz.choices[choiceNumber].feedback
+}
+choice[0].onclick = function () {
+  // 0 番目の選択肢を選択
+  choose(0)
+}
+choice[1].onclick = function () {
+  // 1 番目の選択肢を選択
+  choose(1)
+}
+choice[2].onclick = function () {
+  // 2 番目の選択肢を選択
+  choose(2)
+}
+// reloadQuiz関数 を実行して、クイズを画面に表示する
+reloadQuiz()
