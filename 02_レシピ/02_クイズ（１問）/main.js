efb01d335a52508fb9981aa6c7a2b2aa3694c075
aniosu <ameniosu@au.com>
2024-11-28 14:32:12 +0900

    11/28 14:32

--- 02_レシピ/02_クイズ（１問）/main.js
+++ 02_レシピ/02_クイズ（１問）/main.js
@@ -4,7 +4,7 @@ const quizButton = document.getElementById("quiz-button")
 const feedback = document.getElementById("feedback")
 const choice = []
 // クイズの内容
-const quiz = {
+const quiz1 = {
   text: "この星の名前は何でしょう？",
   image: "Ganymede.jpg",
   choices: [
@@ -23,17 +23,23 @@ const quiz = {
     },
   ],
 }
-
+const quiz2 = {
+  text: "この学校の名前は何でしょう",
+  image: "",
+  choices: [
+    //text: "明治"
+  ],
+}
 // quiz を画面に表示する関数
 const reloadQuiz = function () {
   // 問題文を表示
-  quizText.textContent = "Q. " + quiz.text
+  quizText.textContent = "Q. " + quiz1.text
 
   // 画像を表示
-  quizImage.src = "./images/" + quiz.image
+  quizImage.src = "./images/" + quiz1.image
   for (let i = 0; i < quiz.choices.length; i++) {
     choice[i] = document.createElement("button")
-    choice[i].textContent = quiz.choices[i].text
+    choice[i].textContent = quiz1.choices[i].text
     choice[i].onclick = function () {
       // 0 番目の選択肢を選択
       choose(i)
@@ -45,7 +51,7 @@ const reloadQuiz = function () {
 // choiceNumber番目の選択肢を選択
 const choose = function (choiceNumber) {
   // フィードバックを表示
-  feedback.textContent = quiz.choices[choiceNumber].feedback
+  feedback.textContent = quiz1.choices[choiceNumber].feedback
 }
 
 // reloadQuiz関数 を実行して、クイズを画面に表示する
